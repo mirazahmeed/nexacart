950517ef030a064f2e125fc86ca0a73ec1ab6fb3
mirazahmeed <mirazahmed0619@gmail.com>
2025-11-10 22:38:00 +0600

    pages structure , moved router to its own file, and set up basic routing for home and about pages.

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router';
-import Root from '../Root/Root';
-import Home from '../Home/Home';
-import Men from '../Products/Men/Men';
-import Women from '../Products/Women/Women';
-import Kids from '../Products/Kids/Kids';
-
-
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Home/Home";
+import Men from "../Products/Men/Men";
+import Women from "../Products/Women/Women";
+import Kids from "../Products/Kids/Kids";
 
 export const router = createBrowserRouter([
-  {     path: "/",
+    {
+        path: "/",
         Component: Root,
         children: [
-            {index: true, Component: Home},
-            {path: "men", Component: Men},
-            {path:"women", Component: Women},
-            {path:"kids", Component: Kids},
-
-        ]
-        }
+            { index: true, Component: Home },
+            { path: "men", Component: Men },
+            { path: "women", Component: Women },
+            { path: "kids", Component: Kids },
+        ],
+    },
 ]);
